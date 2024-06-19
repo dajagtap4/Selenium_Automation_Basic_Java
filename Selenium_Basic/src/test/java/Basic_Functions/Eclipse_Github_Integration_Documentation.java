a861acd72c074665c71fe4d797b2407e730752e6
Deepak <deepakgithub4@gmail.com>
2024-06-19 11:23:13 +0530

    Added Eclipse_GIthub_Integration_Docs File

---
+++ Selenium_Basic/src/test/java/Basic_Functions/Eclipse_Github_Integration_Documentation.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package Basic_Functions;
+
+/**
+ * @author Deepak
+ *
+ */
+public class Eclipse_Github_Integration_Documentation {
+
+//	Integrate eclipse with GitHub
+//
+//	Step 1 : Click on search box in eclipse right top corner,
+//	Step 2 : Search Git Repository > click on git repository option,
+//	Step 3 : At bottom left corner Git Repositories box will open, 
+//	Step 4 : At top right navigation bar of 'Git repositories' box,--
+//		 --Click on 'Clone a Git Repository and add the clone to this view' option,
+//	Step 5 : Paste git repo's link to URI textbox, 
+//	>	 Leave 'Authentication' section blank which contains User and password, We will create 
+//		 'Personal access tocken' in git at end for push code in git.
+//
+//	Step 6 : Click on Next > Finish ( Click next > Next until finish )
+//	>	 Respected Repo will show in 'Git Repositories' box.
+//
+//	Step 7 : add project 
+//	Step 8 : Git staging will open at console/terminal section at bottom,
+//	>	 If Git staging not opening at bottom then right click at project > team > commit
+//	Step 9 : Drag files from 'Unstaged changes' to 'Staged Changes'
+//	Step 10: Add Commit Massage,
+//	Step 11: Click on 'Commit and Push' Button at right bottom corner behind Commit button.
+//
+//	DONE
+
+}
