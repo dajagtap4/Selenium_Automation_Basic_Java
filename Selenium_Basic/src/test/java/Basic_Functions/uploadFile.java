3d0c35b2edda9bfee009284ff8106ebb347324a6
Deepak <deepakgithub4@gmail.com>
2024-07-08 12:13:56 +0530

    update uploade file code

--- Selenium_Basic/src/test/java/Basic_Functions/uploadFile.java
+++ Selenium_Basic/src/test/java/Basic_Functions/uploadFile.java
@@ -11,18 +11,17 @@ public class uploadFile {
 
 	@Test
 	public void uploadFileTest() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Deepak\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Deepak\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
 		driver.manage().window().maximize();
 		driver.get("https://tus.io/demo");
-		
-		WebElement browse =driver.findElement(By.id("P0-0"));
-		
+
+		WebElement browse = driver.findElement(By.id("P0-0"));
+
 		Actions actions = new Actions(driver);
-		actions.moveToElement(browse);
-		actions.perform();
-		
+		actions.moveToElement(browse).perform();
+
 		browse.sendKeys("C:\\Automation\\Documentaion\\Java Concepts\\37-50.txt");
 	}
 }
