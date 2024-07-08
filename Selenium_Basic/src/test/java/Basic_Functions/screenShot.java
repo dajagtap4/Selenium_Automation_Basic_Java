dc532539be2863927996b0f74cede35e8d43a5d5
Deepak <deepakgithub4@gmail.com>
2024-07-08 09:13:35 +0530

    screenshot class

---
+++ Selenium_Basic/src/test/java/Basic_Functions/screenShot.java
@@ -0,0 +1,27 @@
+package Basic_Functions;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class screenShot {
+@Test
+	public void takeScreenShot() throws IOException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Deepak\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+		
+		File ref = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(ref, new File(".\\screenshot\\screen.png"));
+		
+		driver.close();
+	}
+}
