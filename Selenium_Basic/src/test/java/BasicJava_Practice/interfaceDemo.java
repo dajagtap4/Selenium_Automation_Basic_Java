9165a80296ca9c2a7f2359f2a675703c9e67ba07
dajagtap4 <deepakgithub4@gmail.com>
2024-09-18 18:31:34 +0530

    Update interfaceDemo.java

--- Selenium_Basic/src/test/java/BasicJava_Practice/interfaceDemo.java
+++ Selenium_Basic/src/test/java/BasicJava_Practice/interfaceDemo.java
@@ -43,3 +43,31 @@ public class interfaceDemo implements Shape{
 		//sh.demoMethod();
 	}
 }
+
+
+It is a way to achieve abstraction and multiple inheritance. 
+
+Interfaces can declare methods without providing their implementation. 
+Classes that implement the interface must provide implementations for all of its methods.
+
+A class can implement multiple interfaces
+
+All variables declared in an interface are implicitly public, static, and final.
+
+Starting from Java 8, interfaces can also contain default methods (with a body) and static methods.
+
+Interfaces cannot be instantiated directly and do not have constructors.
+
+
+Benefits of Using Interfaces
+Abstraction: Interfaces provide a way to define abstract types that can be implemented by any class,
+	promoting loose coupling and flexibility.
+
+Multiple Inheritance: Unlike classes, which can only inherit from one superclass, a class can implement multiple interfaces, 
+	allowing for greater flexibility.
+
+Polymorphism: Interfaces enable polymorphism, allowing methods to use parameters of the interface type, 
+        enabling different implementations to be passed.
+
+Separation of Concerns: They help separate the definition of methods from their implementations, 
+	leading to cleaner and more maintainable code.
