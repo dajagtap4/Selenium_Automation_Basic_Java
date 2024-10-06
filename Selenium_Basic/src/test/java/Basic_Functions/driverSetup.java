706dab485d5cc8c5505a667aad4c9ad82d9ebda3
dajagtap4 <deepakgithub4@gmail.com>
2024-10-06 15:59:39 +0530

    Update driverSetup.java

--- Selenium_Basic/src/test/java/Basic_Functions/driverSetup.java
+++ Selenium_Basic/src/test/java/Basic_Functions/driverSetup.java
@@ -12,5 +12,13 @@ public class driverSetup {
 		driver = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://testautomationpractice.blogspot.com/");
+
+
+		//traditional way
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Deepak\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.globalsqa.com/demo-site/select-dropdown-menu/");
+		driver.manage().window().maximize();
 		}
 	}
