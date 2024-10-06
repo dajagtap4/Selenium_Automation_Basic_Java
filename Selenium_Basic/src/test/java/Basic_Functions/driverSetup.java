6be3432fc92403c57a8a5ebf45c27e5c66064359
dajagtap4 <deepakgithub4@gmail.com>
2024-10-06 15:58:35 +0530

    Update driverSetup.java

--- Selenium_Basic/src/test/java/Basic_Functions/driverSetup.java
+++ Selenium_Basic/src/test/java/Basic_Functions/driverSetup.java
@@ -1,13 +1,16 @@
 package Basic_Functions;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class driverSetup {
 	@Test
 	public static void setUp() {
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\Deepak\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-		
+		WebDriverManager.edgedriver().setup();
+		driver = new EdgeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://testautomationpractice.blogspot.com/");
 		}
-	
 	}
