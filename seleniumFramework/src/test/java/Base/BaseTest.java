22995b4f8d32b7ba94621dba65c37558bcfe673e
dajagtap4 <deepakgithub4@gmail.com>
2024-10-07 16:28:24 +0530

    Update BaseTest.java

--- seleniumFramework/src/test/java/Base/BaseTest.java
+++ seleniumFramework/src/test/java/Base/BaseTest.java
@@ -1,46 +1,50 @@
 package Base;
- 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.Properties;
 
+import java.io.IOException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
- 
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import Pages.PracticePage;
+import Utilities.Utility;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseTest {
- 
-	public static WebDriver driver;
-	public static Properties prop = new Properties();
-	//public static Properties loc = new Properties();
-	public static FileReader fr;
-	public static FileReader fr2;
- 
-	@BeforeTest
-	public void setup() throws IOException {
-		if (driver == null) {
-			fr = new FileReader(System.getProperty("user.dir")+"\\src\\test\\resources\\configFiles\\config.properties");
-			fr2 = new FileReader(System.getProperty("user.dir")+"\\src\\test\\resources\\configFiles\\locators.properties");
-			
-			prop.load(fr);
-			prop.load(fr2);
-		}
-		
-		if (prop.getProperty("browser").equalsIgnoreCase("chrome")) {
-			//WebDriverManager.chromedriver().setup();
-			System.setProperty("webdriver.chrome.driver",
-					"C:\\Users\\Deepak\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
-		
+
+	private static WebDriver driver;
+	public PracticePage practicePage;
+
+	@BeforeClass
+	public void driverInstance() throws IOException {
+		if (Utility.fetchProperty("browser").toString().equalsIgnoreCase("chrome")) 
+		{
+			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
-			driver.manage().window().maximize();
-			driver.get(prop.getProperty("testurl"));
 		}
+		
+		else if (Utility.fetchProperty("browser").toString().equalsIgnoreCase("edge")) 
+		{
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();	
+		} 
+		
+		else if (Utility.fetchProperty("browser").toString().equalsIgnoreCase("firefox")) 
+		{
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+
+		driver.manage().window().maximize();
+		driver.get(Utility.fetchProperty("testurl").toString());
+		practicePage = new PracticePage(driver);
 	}
- 
-	@AfterTest
-	public void tearDown() {
-		driver.close();
-		System.out.println("passed...");
+
+	@AfterClass
+	public void teardown() {
+		if (driver != null) {
+			driver.quit();
+		}
 	}
 }
