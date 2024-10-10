cb92bb4c0a9b2ea5912fdc4302636a3612a14cb2
dajagtap4 <deepakgithub4@gmail.com>
2024-10-10 21:37:52 +0530

    Update SNIPPET_reverseString.java

--- Selenium_Basic/src/test/java/BasicJava_Practice/SNIPPET_reverseString.java
+++ Selenium_Basic/src/test/java/BasicJava_Practice/SNIPPET_reverseString.java
@@ -5,12 +5,16 @@ public class SNIPPET_reverseString {
 	public static void main(String[] args) {
 		String string1 = "Pune";
 		String string2 = "";
-		
+
+		// Method 1
 		for(int i=0;i<string1.length();i++)
 		{
 			string2 = string1.charAt(i)+string2;
 		}
 			System.out.println(string2);
-	}
 
+		// Method 2
+        	String reverseString = new StringBuilder(string1).reverse().toString();
+        	System.out.println(reverseString);
+	}
 }
