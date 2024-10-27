6a5e9cf1bf916cfa635a723a768a0a35c2d4a8cc
dajagtap4 <deepakgithub4@gmail.com>
2024-10-27 18:51:06 +0530

    Update SNIPPET_duplicateInString.java

--- Selenium_Basic/src/test/java/BasicJava_Practice/SNIPPET_duplicateInString.java
+++ Selenium_Basic/src/test/java/BasicJava_Practice/SNIPPET_duplicateInString.java
@@ -1,16 +1,40 @@
- String name = "deepak"; //depak Jgt
+ 	String name = "Deepak";  //e
         String result = "";
-        Set<Character> set  = new HashSet<Character>();
         
-        for(int i=0;i<name.length();i++){ 
-            Character c = name.charAt(i);  
-            if(set.contains(c)){ 
-                if(!result.contains(String.valueOf(c))){
-                    result = result +c;
-                }
-            }
-             else{
-                    set.add(c);  
-                }
-        }
+        Set<Character> set = new HashSet<Character>();
+        
+        for(int i=0;i<name.length();i++){
+            Character c = name.charAt(i);
+            
+            if(set.contains(c))
+		             {
+                 if(!result.contains(String.valueOf(c)))
+		                 {
+                      result = result + c;
+                   }
+               }//1st if
+	          	else
+	             {
+                set.add(c);
+              }//else
+        }//for
+
         System.out.println(result);
+
+
+
+	String name = "Deepak";    //Depak
+	
+	Set<Character> set = new HashSet<Character>();
+	StringBuffer sf = new StringBuffer();
+
+	for(int i=0;i<str.length();i++) {
+	    Character c = str.charAt(i);
+
+		if(!set.contains(c)) {
+		  set.add(c);
+		  sf.append(c);
+		}//if
+		
+	  }//for
+	System.out.println(sf);
