944c71aa779d7e9d7ae464af169edcfb0b83f653
dajagtap4 <deepakgithub4@gmail.com>
2024-10-27 17:21:52 +0530

    Update SNIPPET_duplicateInString.java

--- Selenium_Basic/src/test/java/BasicJava_Practice/SNIPPET_duplicateInString.java
+++ Selenium_Basic/src/test/java/BasicJava_Practice/SNIPPET_duplicateInString.java
@@ -1,19 +1,1 @@
- //REMOVE DUPLICATES FROM STRING
 
-String name = "Deepak Jagtap"; //Depak Jgt
-        String result = "";
-        
-        for (int i = 0; i < name.length(); i++) {
-            boolean isDuplicate = false;
-            for (int j = 0; j < result.length(); j++) {
-                
-                if (name.charAt(i) == result.charAt(j)) {
-                    isDuplicate = true;
-                    break;
-                }
-            }
-             if (!isDuplicate) {
-                result += name.charAt(i);
-            }
-    }
-    System.out.println(result);
