98dca1b32148d012c8cacc056224e5f21d1f121b
dajagtap4 <deepakgithub4@gmail.com>
2024-10-27 18:21:49 +0530

    Update SNIPPET_duplicateInString.java

--- Selenium_Basic/src/test/java/BasicJava_Practice/SNIPPET_duplicateInString.java
+++ Selenium_Basic/src/test/java/BasicJava_Practice/SNIPPET_duplicateInString.java
@@ -1,1 +1,16 @@
-
+ String name = "deepak"; //depak Jgt
+        String result = "";
+        Set<Character> set  = new HashSet<Character>();
+        
+        for(int i=0;i<name.length();i++){ 
+            Character c = name.charAt(i);  
+            if(set.contains(c)){ 
+                if(!result.contains(String.valueOf(c))){
+                    result = result +c;
+                }
+            }
+             else{
+                    set.add(c);  
+                }
+        }
+        System.out.println(result);
