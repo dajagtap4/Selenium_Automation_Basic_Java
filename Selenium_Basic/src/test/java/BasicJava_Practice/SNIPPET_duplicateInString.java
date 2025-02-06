1dbb03c058463f6af7e322e9c46a95accd8da760
Deepak Jagtap <deepakgithub4@gmail.com>
2025-02-06 14:47:19 +0530

    Update SNIPPET_duplicateInString.java

--- Selenium_Basic/src/test/java/BasicJava_Practice/SNIPPET_duplicateInString.java
+++ Selenium_Basic/src/test/java/BasicJava_Practice/SNIPPET_duplicateInString.java
@@ -1,40 +1,30 @@
- 	String name = "Deepak";  //e
+import java.util.HashSet;
+import java.util.Set;
+
+class Main {
+    public static void main(String[] args) {
+        String name = "Deepak";  //e
         String result = "";
         
         Set<Character> set = new HashSet<Character>();
         
-        for(int i=0;i<name.length();i++){
+        for(int i=0;i<name.length();i++)
+        {
             Character c = name.charAt(i);
             
             if(set.contains(c))
-		             {
+	     {
                  if(!result.contains(String.valueOf(c)))
-		                 {
+		 {
                       result = result + c;
-                   }
-               }//1st if
-	          	else
+                 }
+             }//1st if
+	          else
 	             {
-                set.add(c);
-              }//else
+                       set.add(c);
+                     }//else
         }//for
-
-        System.out.println(result);
-
-
-
-	String name = "Deepak";    //Depak
-	
-	Set<Character> set = new HashSet<Character>();
-	StringBuffer sf = new StringBuffer();
-
-	for(int i=0;i<str.length();i++) {
-	    Character c = str.charAt(i);
-
-		if(!set.contains(c)) {
-		  set.add(c);
-		  sf.append(c);
-		}//if
-		
-	  }//for
-	System.out.println(sf);
+        
+        System.out.println(result);// Output will be 'e'
+    }
+}
