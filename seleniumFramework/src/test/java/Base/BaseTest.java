336a57009f769633c7c258f4f3dc2cb6a11c37af
Deepak Jagtap <deepakgithub4@gmail.com>
2025-03-13 11:30:18 +0530

    Update BaseTest.java

--- seleniumFramework/src/test/java/Base/BaseTest.java
+++ seleniumFramework/src/test/java/Base/BaseTest.java
@@ -1,44 +1,49 @@
-package Base;
+package base;
 
 import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import Pages.PracticePage;
-import Utilities.Utility;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
+import pages.TendablePgae;
+import pages.practicePOM;
+import utilities.utility;
 
 public class BaseTest {
 
-	private static WebDriver driver;
-	public PracticePage practicePage;
+	protected WebDriver driver;
+	public TendablePgae tendablePgae;
+	public practicePOM practice;
 
 	@BeforeClass
 	public void driverInstance() throws IOException {
-		if (Utility.fetchProperty("browser").toString().equalsIgnoreCase("chrome")) 
+		if (utility.fetchConfigProperty("browser").toString().equalsIgnoreCase("chrome")) 
 		{
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 		}
 		
-		else if (Utility.fetchProperty("browser").toString().equalsIgnoreCase("edge")) 
+		else if (utility.fetchConfigProperty("browser").toString().equalsIgnoreCase("edge")) 
 		{
 			WebDriverManager.edgedriver().setup();
 			driver = new EdgeDriver();	
 		} 
 		
-		else if (Utility.fetchProperty("browser").toString().equalsIgnoreCase("firefox")) 
+		else if (utility.fetchConfigProperty("browser").toString().equalsIgnoreCase("firefox")) 
 		{
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 		}
 
 		driver.manage().window().maximize();
-		driver.get(Utility.fetchProperty("testurl").toString());
-		practicePage = new PracticePage(driver);
+		driver.get(utility.fetchConfigProperty("testurl").toString());
+		tendablePgae = new TendablePgae(driver);
+		practice = new practicePOM(driver);  
 	}
 
 	@AfterClass
