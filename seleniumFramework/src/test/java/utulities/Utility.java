bb897c512afd731389dc828874efed71af2dbd11
Deepak Jagtap <deepakgithub4@gmail.com>
2025-03-13 11:31:45 +0530

    Update Utility.java

--- seleniumFramework/src/test/java/utulities/Utility.java
+++ seleniumFramework/src/test/java/utulities/Utility.java
@@ -1,14 +1,21 @@
-package Utilities;
+package utilities;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
-public class Utility {
-	public static Object fetchProperty(String key) throws IOException    {
-		FileInputStream file = new FileInputStream("./src/test/resources/configfiles/config.properties");
+public class utility {
+	public static Object fetchConfigProperty(String key) throws IOException    {
+		FileInputStream file = new FileInputStream("./src/main/resources/configfiles/config.properties");
 		Properties prop = new Properties();
 		prop.load(file);
 		return prop.get(key);	
+	}
+	
+	public static String fetchLocatorProperty(String key) throws IOException    {
+		FileInputStream file = new FileInputStream("./src/main/resources/configfiles/locator.properties");
+		Properties prop = new Properties();
+		prop.load(file);
+		return prop.get(key).toString();	
 	}
 }
