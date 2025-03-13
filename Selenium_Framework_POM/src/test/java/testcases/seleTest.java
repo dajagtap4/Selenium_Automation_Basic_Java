6d49768cfd56af119b11bf16400eba1c847964c5
Deepak Jagtap <deepakgithub4@gmail.com>
2025-03-13 14:12:57 +0530

    Update seleTest.java

--- Selenium_Framework_POM/src/test/java/testcases/seleTest.java
+++ Selenium_Framework_POM/src/test/java/testcases/seleTest.java
@@ -12,9 +12,31 @@ public class seleTest extends BaseTest {
 	@Test(groups = "title")
 	public void verifyPageTitle() throws IOException {
 		String actualTitle = practice.getPageTitle();
-		String expectedTitle = utilities.fetchConfigProperty("expectedTitle").toString();
+		String expectedTitle = utility.fetchConfigProperty("expectedTitle").toString();
 
 		Assert.assertEquals(actualTitle, expectedTitle, "Page title does not match!");
+	}
+
+	@Test(groups = "title")
+	public void verifyIncorrectPageTitle() throws IOException {
+		String actualTitle = practice.getPageTitle();
+		String incorrectTitle = utility.fetchConfigProperty("wrongtitle").toString(); // Deliberately incorrect
+		
+		Assert.assertNotEquals(actualTitle, incorrectTitle, "Test failed: Unexpected match with incorrect title!");
+	}
+
+	@Test(groups = "title")
+	public void verifyTitleIsNotEmpty() {
+		String actualTitle = practice.getPageTitle();
+
+		Assert.assertFalse(actualTitle.isEmpty(), "Test failed: Page title is empty");
+	}
+
+	@Test(groups = "title")
+	public void verifyTitleDoesNotContainUnexpectedText() {
+		String actualTitle = practice.getPageTitle();
+
+		Assert.assertFalse(actualTitle.contains("error"), "Test failed: Page title contains 'Error'");
 	}
 
 	@Test
@@ -22,5 +44,27 @@ public class seleTest extends BaseTest {
 		String enteredName1 = practice.enterName("Deepak");
 		String enteredName2 = practice.getEnteredNameInNameInputBox();
 		Assert.assertEquals(enteredName1, enteredName2, "Test failed: Entered name does not match input value!");
+	}
+
+	@Test
+	public void verifyUserCannotEnterEmptyName() {
+		practice.enterName("");
+		String enteredName2 = practice.getEnteredNameInNameInputBox();
+		Assert.assertTrue(enteredName2.isEmpty(), "Test failed: Entered Empty input value!");
+	}
+
+	@Test
+	public void verifyUserCannotEnterSpecialCharacters() {
+		String enteredName1 = practice.enterName("!@#$%");
+		String enteredName2 = practice.getEnteredNameInNameInputBox();
+		Assert.assertEquals(enteredName1, enteredName2, "Test failed: Special characters should not be accepted!");
+	}
+
+	@Test
+	public void verifyUserCannotEnterExcessivelyLongName() {
+		String enteredName1 = practice.enterName("Deepak".repeat(100));
+		String enteredName2 = practice.getEnteredNameInNameInputBox();
+
+		Assert.assertNotEquals(enteredName1, enteredName2, "Test failed: Input should have a character limit!");
 	}
 }
